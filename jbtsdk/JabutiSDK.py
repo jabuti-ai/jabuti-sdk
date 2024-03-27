4aa7f386ad84c445a198cc7aa6c6988ac83444f4
Thales Araujo <thales.araujoinmetrics.com.br>
2024-03-27 11:22:21 -0300

    Implemented callbacks

--- jbtsdk/JabutiSDK.py
+++ jbtsdk/JabutiSDK.py
@@ -3,14 +3,18 @@ import requests
 import json
 
 class JabutiSDK():
-    def __init__(self, api_url='', api_key=''):
+    def __init__(self, api_url='', api_key='', callbacks=[]):
         self.api_url = api_url if api_url else os.getenv("JBT_SDK_API_URL")
         self.api_key = api_key if api_key else os.getenv("JBT_SDK_API_KEY")
+        self.callbacks = callbacks
         
     def invoke(self, input, context):
         try:
             headers = {'x-api-key': self.api_key}
             response = requests.post(self.api_url, data=json.dumps({"input": f"{str(input)}", "context": f"{str(context)}"}), headers=headers)
+            if self.callbacks:
+                for cb in self.callbacks:
+                    cb.on_llm_new_token(token=response)
             return response.text
         except Exception as e:
             print(f"Error: {e}")
